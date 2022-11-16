80c3298eb8894e64d23c56c45f52953a5e75702c
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-11-16 18:16:24 -0300

    "testando método divisão da classe Calculadora"

--- src/Principal.java
+++ src/Principal.java
@@ -4,5 +4,8 @@ public class Principal {
         Calculadora c = new Calculadora();
         System.out.println(c.soma(10, 10));
 
+        System.out.println("Testando")
+        System.out.println(c.divisao(100, 4));
+
     }
 }
